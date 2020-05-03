f0c2c24ec983b28c5cc861041fba2d1a72a9e7ca
Eduardo Silva <eduardo__lps@hotmail.com>
2020-05-03 22:59:27 +0100

    Create app structure

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+const server = app.listen(process.env.PORT || 8080, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
