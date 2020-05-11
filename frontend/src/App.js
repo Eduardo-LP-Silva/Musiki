7d4227cf8a3120837b67fef17b596c594ee19355
pedrovfranco <yurbypedro@hotmail.com>
2020-05-11 16:59:29 +0100

    Add stuff that didn't make it to the last commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 import Navbar from './components/navbar/navbar';
 import Settings from './components/settings/settings';
+import Graph from './components/graph/graph';
+import {nodes, links} from './components/graph/miserables.json'
 import './App.css';
 
 class App extends Component {
@@ -10,7 +12,8 @@ class App extends Component {
     super(props);
 
     this.state = {
-      settings: 0
+      settings: 0,
+      graphData: {nodes: nodes, links: links}
     };
 
     this.toggleSettings = this.toggleSettings.bind(this);
@@ -21,9 +24,16 @@ class App extends Component {
       <div id="content">
         <Navbar/>
         <FontAwesomeIcon id="settings-icon" icon={faCog} onClick={this.toggleSettings}/>
-        <Settings opacity={this.state.settings}/>
+        <Settings opacity={this.state.settings} callback={this.addGraphNode}/>
+        <Graph graphData={this.state.graphData}/>
       </div>
     );
+  }
+
+  addGraphNode(foobar) {
+    // TODO:
+    console.log(foobar);
+
   }
 
   toggleSettings() {
