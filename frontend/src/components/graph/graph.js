6a67b887f9167e57c3c949f294e55f6eafc1de92
Alexandra Mendes <up201604741@fe.up.pt>
2020-05-18 01:19:39 +0100

    Graph design done
    ?

--- frontend/src/components/graph/graph.js
+++ frontend/src/components/graph/graph.js
@@ -7,17 +7,29 @@ class Graph extends Component {
   render() {
     return (
       <ForceGraph2D
-        class="col-md-10"
         graphData={this.props.graphData}
         onNodeClick={this.props.onNodeClick}
         nodeLabel="id"
+        linkColor={() => 'rgba(255,255,255,0.7)'}
         linkLabel="link"
-        width={1246}
+        width={ window.innerWidth * 0.8}
         nodeCanvasObject={({ id, x, y }, ctx, globalScale) => {
-          ctx.fillStyle = "rgba(255,255,255, 0.8)";
+          const label = id;
+          const fontSize = 15/globalScale;
+          ctx.font = `${fontSize}px arial`;
+          ctx.fillStyle = "rgba(255,255,255, 1)";
+          ctx.shadowBlur = "1";
+          ctx.shadowColor = "rgba(0, 0 ,0 , 0.25)";
+          ctx.shadowOffsetX = "4";
+          ctx.shadowOffsetY = "4";
           ctx.beginPath();
-          ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
+          ctx.arc(x, y, ctx.measureText(label).width * 0.8, 0, 2 * Math.PI, false);
           ctx.fill();
+          ctx.fillStyle = '#808080';
+          ctx.shadowColor = "rgba(0, 0, 0, 0)";
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.fillText(label, x, y);
         }}
       />
     );
