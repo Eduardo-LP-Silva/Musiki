16407eca6e7c0597ed3473cf30a3aad4aef69432
Alexandra Mendes <up201604741@fe.up.pt>
2020-05-19 21:42:38 +0100

    merda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCog } from '@fortawesome/free-solid-svg-icons';
-import Navbar from './components/navbar/navbar';
-import Settings from './components/settings/settings';
-import Graph from './components/graph/graph';
-import {nodes, links} from './components/graph/miserables.json';
-import './App.css';
+import { faCog } from "@fortawesome/free-solid-svg-icons";
+import Navbar from "./components/navbar/navbar";
+import Settings from "./components/settings/settings";
+import Graph from "./components/graph/graph";
+import "./App.css";
 
-const requests = require('./components/requests/requests');
+const requests = require("./components/requests/requests");
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-    settings: 0,
-    graphData: {nodes: [], links: []},
-    selectedNode: {type: "none", id: ""}, // id
-    nodeFilters: new Map(),
-    groupIndex: 1,
-    nodeInfo: undefined,
+      settings: 0,
+      graphData: { nodes: [], links: [] },
+      selectedNode: { type: "none", id: "" }, // id
+      nodeFilters: new Map(),
+      groupIndex: 1,
+      nodeInfo: undefined
     };
 
     this.toggleSettings = this.toggleSettings.bind(this);
@@ -28,36 +27,64 @@ class App extends Component {
     this.removeNodeFilter = this.removeNodeFilter.bind(this);
     this.search = this.search.bind(this);
     this.onNodeClick = this.onNodeClick.bind(this);
+    this.onNodeHover = this.onNodeHover.bind(this);
+    this.paintRing = this.paintRing.bind(this);
+  }
+
+  paintRing(node, ctx) {
+    console.log("HELO");
+    // paint highlighted nodes
+    ctx.beginPath();
+    ctx.arc(
+      node.x,
+      node.y,
+      ctx.measureText(node.id).width * 0.8,
+      0,
+      2 * Math.PI,
+      false
+    );
+    ctx.fillStyle = "orange";
+    ctx.fill();
   }
 
   render() {
     return (
       <div id="content">
-        <Navbar 
-          search={this.search} 
+        <Navbar
+          search={this.search}
           selectedNode={this.state.selectedNode}
           nodeFilters={this.state.nodeFilters}
-          setNodeFilters={this.removeNodeFilter}/>
-          <FontAwesomeIcon id="settings-icon" icon={faCog} onClick={this.toggleSettings}/>
+          setNodeFilters={this.removeNodeFilter}
+        />
+        <FontAwesomeIcon
+          id="settings-icon"
+          icon={faCog}
+          onClick={this.toggleSettings}
+        />
         <Row>
-          <Col md={2} >
-          <Settings 
-            selectedNode={this.state.selectedNode} 
-            opacity={this.state.settings} 
-            callback={this.addGraphNode}
-            filters={this.state.nodeFilters}
-            addFilter={this.addNodeFilter}
-            removeFilter={this.removeNodeFilter}
-            nodeInfo={this.state.nodeInfo}
-            ref="settings"
-
-          />
+          <Col md={2}>
+            <Settings
+              selectedNode={this.state.selectedNode}
+              opacity={this.state.settings}
+              callback={this.addGraphNode}
+              filters={this.state.nodeFilters}
+              addFilter={this.addNodeFilter}
+              removeFilter={this.removeNodeFilter}
+              nodeInfo={this.state.nodeInfo}
+              ref="settings"
+            />
           </Col>
           <Col md={10} className="justify-content-center">
-			 <Graph 
-			   graphData={this.state.graphData}
-				 onNodeClick={this.onNodeClick}
-			 />
+            <Graph
+              id="graph"
+              graphData={this.state.graphData}
+              nodeCanvasObjectMode={(node) =>
+                (node === this.state.selectedNode) ? "before" : undefined
+              }
+              nodeCanvasObject={this.paintRing}
+              onNodeClick={this.onNodeClick}
+              onNodeHover={this.onNodeHover}
+            />
           </Col>
         </Row>
       </div>
@@ -66,107 +93,99 @@ class App extends Component {
 
   componentDidMount() {
     requests.get("nodeInfo", undefined, (result) => {
-      this.setState({nodeInfo: result});
-    })
-
+      this.setState({ nodeInfo: result });
+    });
   }
 
-	search(searchString) {
-		console.log(searchString);
+  search(searchString) {
+    console.log(searchString);
 
-    requests.post("search", { search_string: searchString, node_type: this.state.selectedNode.type}, (result) => {
-      console.log(result);
+    requests.post(
+      "search",
+      { search_string: searchString, node_type: this.state.selectedNode.type },
+      (result) => {
+        console.log(result);
 
-      if (result.type.toUpperCase() !== "NULL") {
-        result.group = this.state.groupIndex;
-      // this.setState({groupIndex: this.state.groupIndex+1});
-        this.addNode(result);
+        if (result.type.toUpperCase() !== "NULL") {
+          result.group = this.state.groupIndex;
+          // this.setState({groupIndex: this.state.groupIndex+1});
+          this.addNode(result);
+        }
       }
+    );
+  }
 
-    });
-	}
-	
   addNode(node) {
-		this.setState({graphData: {nodes: this.state.graphData.nodes.concat([node]), links: this.state.graphData.links}});
+    this.setState({
+      graphData: {
+        nodes: this.state.graphData.nodes.concat([node]),
+        links: this.state.graphData.links,
+      },
+    });
   }
 
   addNodeChildren(parentNode, newNode) {
-
     this.addNode(newNode);
 
-    let link = {source: parentNode, target: newNode.id, value: 1};
+    let link = { source: parentNode, target: newNode.id, value: 1 };
 
-    this.setState({graphData: {nodes: this.state.graphData.nodes, links: this.state.graphData.links.concat([link])}});
-
+    this.setState({
+      graphData: {
+        nodes: this.state.graphData.nodes,
+        links: this.state.graphData.links.concat([link]),
+      },
+    });
   }
 
   addNodeFilter(filter) {
     const filters = this.state.nodeFilters;
 
-    if(filters.has(this.state.selectedNode.id)) {
+    if (filters.has(this.state.selectedNode.id)) {
       const selectedFilters = filters.get(this.state.selectedNode.id);
 
-      if(!selectedFilters.includes(filter)) {
+      if (!selectedFilters.includes(filter)) {
         selectedFilters.push(filter);
         filters.set(this.state.selectedNode.id, selectedFilters);
-      }
-      else {
-        console.log(`The selected node ${this.state.selectedNode.id} already has ${filter} filter active!`);
+      } else {
+        console.log(
+          `The selected node ${this.state.selectedNode.id} already has ${filter} filter active!`
+        );
         return;
       }
-    } 
-    else
-      filters.set(this.state.selectedNode.id, [filter]);
+    } else filters.set(this.state.selectedNode.id, [filter]);
 
-    this.setState({nodeFilters: filters});
+    this.setState({ nodeFilters: filters });
     console.log(this.state.nodeFilters);
 
     this.addNodeFilterGraph(filter);
   }
 
   addNodeFilterGraph(filter) {
-
     let filters = this.state.nodeInfo[this.state.selectedNode.type].filters;
     for (let i = 0; i < filters.length; i++) {
       if (filters[i].name.toUpperCase() === filter.toUpperCase()) {
-
         filter = filters[i];
 
         if (!filter.reverse) {
-          requests.get("values", {
-            entities: this.state.selectedNode.id,
-            properties: filter.property,
-          }, (result) => {
+          requests.get(
+            "values",
+            {
+              entities: this.state.selectedNode.id,
+              properties: filter.property,
+            },
+            (result) => {
               let bindings = result.results.bindings;
 
               for (let i = 0; i < bindings.length; i++) {
-                let link = bindings[i][filter.property.replace(':', '')].value;
+                let link = bindings[i][filter.property.replace(":", "")].value;
 
-                this.addNodeChildren(this.state.selectedNode.id, {id: link.substr(link.lastIndexOf('/')+1), type:"none"});
+                this.addNodeChildren(this.state.selectedNode.id, {
+                  id: link.substr(link.lastIndexOf("/") + 1),
+                  type: "none",
+                });
               }
-          })
-        }
-        else {
-          requests.get("entities", {
-            value: `${this.state.selectedNode.id},${filter.property}`,
-            ofilter: filter.validationKey,
-          }, (result) => {
-              let bindings = result.results.bindings;
-				  let added = [];
-
-              for (let i = 0; i < bindings.length; i++) {
-					let entityName =  bindings[i]["entities"].value
-					entityName = entityName.substr(entityName.lastIndexOf('/')+1);
-					let link = bindings[i][filter.validationKey.replace(':', '')]?.value;
-
-					 if (!added.includes(entityName) && link != undefined && link.toUpperCase().includes(filter.validationValue.toUpperCase())) {
-						added.push(entityName);
-						
-						// TODO: get type of child node
-						this.addNodeChildren(this.state.selectedNode.id, {id: entityName, type:"none"});
-					 }
-              }
-          })
+            }
+          );
         }
 
         return;
@@ -177,44 +196,47 @@ class App extends Component {
   removeNodeFilter(filter) {
     const filters = this.state.nodeFilters;
 
-    if(filters.has(this.state.selectedNode.id)) {
+    if (filters.has(this.state.selectedNode.id)) {
       let selectedFilters = filters.get(this.state.selectedNode.id);
 
-      if(selectedFilters.includes(filter)) {
-        selectedFilters = selectedFilters.filter(f => f !== filter);
+      if (selectedFilters.includes(filter)) {
+        selectedFilters = selectedFilters.filter((f) => f !== filter);
         filters.set(this.state.selectedNode.id, selectedFilters);
-        this.setState({nodeFilters: filters});
-      }
-      else
-        console.log(`The selected node ${this.state.selectedNode.id} doesn't have ${filter} filter active!`);
-    } 
-    else
-      console.log(`The selected node ${this.state.selectedNode.id} doesn't have any filters active!`);
-      console.log(this.state.nodeFilters);
+        this.setState({ nodeFilters: filters });
+      } else
+        console.log(
+          `The selected node ${this.state.selectedNode.id} doesn't have ${filter} filter active!`
+        );
+    } else
+      console.log(
+        `The selected node ${this.state.selectedNode.id} doesn't have any filters active!`
+      );
+    console.log(this.state.nodeFilters);
   }
 
   setSelectedNodeFilters(filters) {
-	 const sn = this.state.selectedNode;
-	 
-	 sn.filters = filters;
+    const sn = this.state.selectedNode;
 
-	 this.setState({selectedNode: sn});
+    sn.filters = filters;
+
+    this.setState({ selectedNode: sn });
   }
 
   toggleSettings() {
-	 this.setState({settings: 1 - this.state.settings});
+    this.setState({ settings: 1 - this.state.settings });
   }
-
 
   onNodeClick(node) {
     console.log(node);
-    
-    this.setState({selectedNode: node});
 
-    this.refs.settings.setFilters();
+    this.setState({ selectedNode: node });
   }
 
-  ApplyFilter
+  onNodeHover(node) {
+   
+    if (node)   
+      this.state.selectedNode = node;
+  }
 }
 
 export default App;
--- frontend/src/components/graph/graph.js
+++ frontend/src/components/graph/graph.js
@@ -3,19 +3,19 @@ import { ForceGraph2D } from "react-force-graph";
 import "./graph.css";
 
 class Graph extends Component {
-
   render() {
     return (
       <ForceGraph2D
         graphData={this.props.graphData}
         onNodeClick={this.props.onNodeClick}
+        onNodeHover={this.props.onNodeHover}
         nodeLabel="id"
-        linkColor={() => 'rgba(255,255,255,0.7)'}
+        linkColor={() => "rgba(255,255,255,0.7)"}
         linkLabel="link"
-        width={ window.innerWidth * 0.8}
+        width={window.innerWidth * 0.8}
         nodeCanvasObject={({ id, x, y }, ctx, globalScale) => {
           const label = id;
-          const fontSize = 15/globalScale;
+          const fontSize = 15 / globalScale;
           ctx.font = `${fontSize}px arial`;
           ctx.fillStyle = "rgba(255,255,255, 1)";
           ctx.shadowBlur = "1";
@@ -23,12 +23,19 @@ class Graph extends Component {
           ctx.shadowOffsetX = "4";
           ctx.shadowOffsetY = "4";
           ctx.beginPath();
-          ctx.arc(x, y, ctx.measureText(label).width * 0.8, 0, 2 * Math.PI, false);
+          ctx.arc(
+            x,
+            y,
+            ctx.measureText(label).width * 0.6,
+            0,
+            2 * Math.PI,
+            false
+          );
           ctx.fill();
-          ctx.fillStyle = '#808080';
+          ctx.fillStyle = "#808080";
           ctx.shadowColor = "rgba(0, 0, 0, 0)";
-          ctx.textAlign = 'center';
-          ctx.textBaseline = 'middle';
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
           ctx.fillText(label, x, y);
         }}
       />
