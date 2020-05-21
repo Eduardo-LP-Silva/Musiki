5d3e147ea395f0e3c7bb3dbe926b711dc61d18c1
Alexandra Mendes <up201604741@fe.up.pt>
2020-05-21 11:20:48 +0100

    Click animation done

--- frontend/src/components/graph/graph.js
+++ frontend/src/components/graph/graph.js
@@ -8,12 +8,14 @@ class Graph extends Component {
 
     this.state = {
       hoveredNode: null,
-      radius: null
+      radius: null,
     };
 
     this.onNodeClick = this.onNodeClick.bind(this);
     this.onNodeHover = this.onNodeHover.bind(this);
+    this.clickAnimation = this.clickAnimation.bind(this);
     this.styleNodes = this.styleNodes.bind(this);
+
   }
 
   render() {
@@ -23,7 +25,7 @@ class Graph extends Component {
         nodeLabel="id"
         linkColor={() => "rgba(255,255,255,0.7)"}
         linkLabel="link"
-        nodeRelSize = {this.radius}
+        nodeRelSize={this.radius}
         width={window.innerWidth * 0.8}
         onNodeClick={this.onNodeClick}
         onNodeHover={this.onNodeHover}
@@ -32,14 +34,21 @@ class Graph extends Component {
     );
   }
 
+  
+
   styleNodes(node, ctx, globalScale) {
-    
+
     const label = node.id;
     const fontSize = 15 / globalScale;
-    const size = node === (this.state.hoveredNode || this.props.selectedNode) ? 0.65 : 0.6;
-   
+    const size =
+      node == (this.state.hoveredNode || this.props.selectedNode) ? 0.75 : 0.7;
+
+    //Node design properties
     ctx.font = `${fontSize}px arial`;
-    ctx.fillStyle = node === (this.state.hoveredNode || this.props.selectedNode) ? 'white' : 'rgba(255,255,255,0.8)';
+    ctx.fillStyle =
+      node == (this.state.hoveredNode || this.props.selectedNode)
+        ? "white"
+        : "rgba(255,255,255,0.8)";
     ctx.shadowBlur = "1";
     ctx.shadowColor = "rgba(0, 0 ,0 , 0.25)";
     ctx.shadowOffsetX = "4";
@@ -56,31 +65,67 @@ class Graph extends Component {
       false
     );
     ctx.fill();
-    ctx.fillStyle = node === this.props.selectedNode ? '#595959' : "#808080";
+
+    ctx.fillStyle = node === this.props.selectedNode ? "#595959" : "#808080";
     ctx.shadowColor = "rgba(0, 0, 0, 0)";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillText(label, node.x, node.y);
+    ctx.save();
+   
+    //Click Animation
+    this.clickAnimation(node, ctx, label, size);
+    window.requestAnimationFrame(this.clickAnimation);
+
+  }
+
+  clickAnimation(node, ctx, label, size) {
+
+    var time = new Date();
+  
+    if (node === this.props.selectedNode) {
+      ctx.strokeStyle = "white";
+      ctx.shadowColor = "rgba(0, 0, 0, 0)";
+      ctx.shadowBlur = "1";
+      ctx.shadowColor = "rgba(0, 0 ,0 , 0.25)";
+      ctx.shadowOffsetX = "4";
+      ctx.shadowOffsetY = "4";
+      ctx.lineWidth = 1;
+      ctx.setLineDash([((2* Math.PI *ctx.measureText(label).width * size)/5), ((2* Math.PI *ctx.measureText(label).width * size)/5)]);
+      ctx.translate(node.x, node.y);
+      ctx.rotate(
+        ((2 * Math.PI) / 6) * time.getSeconds() +
+          ((2 * Math.PI) / 6000) * time.getMilliseconds()
+      );
+      ctx.translate(-node.x, -node.y);
+      ctx.beginPath();
+      ctx.arc(
+        node.x,
+        node.y,
+        ctx.measureText(label).width * (size + 0.15),
+        0,
+        2 * Math.PI,
+        false
+      );
+      ctx.stroke();
+      window.requestAnimationFrame(this.clickAnimation);
+      ctx.restore();
+    }
   }
 
   onNodeClick(node) {
     console.log(node);
-    if(node && this.props.selectedNode !== node) {
-      if(!node.hasOwnProperty('activeFilters'))
-        node.activeFilters = [];
+    if (node && this.props.selectedNode !== node) {
+      if (!node.hasOwnProperty("activeFilters")) node.activeFilters = [];
     }
-      this.props.setSelectedNode(node);
+    this.props.setSelectedNode(node);
     //else maybe ?
   }
 
   onNodeHover(node) {
-
-    if(node) {
-      this.setState({hoveredNode: node});
-    }
-    else
-      this.setState({hoveredNode: null});
-      
+    if (node) {
+      this.setState({ hoveredNode: node });
+    } else this.setState({ hoveredNode: null });
   }
 }
 
